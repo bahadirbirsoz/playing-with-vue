34ec0b8ca614c65b3f4f1bbbef4a847e2b37c4fa
Bahadır Birsöz <bahadirbirsoz@gmail.com>
2021-12-24 11:22:07 +0300

    Added vuetify

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,28 +1,16 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" @btnclick="hwButtonClicked"/>
-    <HelloWorld msg="I am a prop"/>
-    <HelloWorld msg="You can use me with @prop"/>
-    <HelloWorld msg="When prop changes, component re-renders"/>
-
-  </div>
+  <hello-world />
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'vue-property-decorator';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+  import Vue from 'vue'
+  import HelloWorld from '../components/HelloWorld.vue'
 
-@Component({
-  components: {
-    HelloWorld,
-  },
-})
-export default class Home extends Vue {
+  export default Vue.extend({
+    name: 'Home',
 
-  hwButtonClicked(){
-    alert("asd");
-  }
-
-}
+    components: {
+      HelloWorld,
+    },
+  })
 </script>
--- vue.config.js
+++ vue.config.js
@@ -1,13 +1,17 @@
 module.exports = {
     lintOnSave: false,
     runtimeCompiler:true,
+
     devServer: {
         disableHostCheck:true,
         proxy: {
             "/api/": {
-                //target: ""
+                target: "https://www.relabase.com/api/"
             }
         }
-    }
+    },
 
+    transpileDependencies: [
+      'vuetify'
+    ]
 }
